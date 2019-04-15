9e75d11b6b761b57c3b0c1c40639263f0f68f96d
ny2112 <47991283+ny2112@users.noreply.github.com>
2019-04-15 17:18:40 +0100

    added pins

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $("#britishpin").click(function(){
-  $("#britishfood").toggleClass("hidden")
-})
+  $("#britishfood").toggleClass("hidden");
+});
+$(#"chinapin").click(function(){
+  $("#chinesefood").toggleClass("hidden");
+});
+$(#"morrocopin").click(function(){
+  $("#morrocanfood").toggleClass("hidden");
+});
+$(#"turkeypin").click(function(){
+  $("#turkishfood").toggleClass("hidden");
+});
+$(#"pakistanpin").click(function(){
+  $("#pakistanifood").toggleClass("hidden")
+});
+$(#"palestinepin").click(function(){
+  $("#palestinianfood").toggleClass("hidden")
+});
+$(#"thaipin").click(function(){
+  $("#thaifood").toggleClass("hidden")
+});
+$(#"japanpin").click(function(){
+  $("#japnesefood").toggleClass("hidden")
+});
